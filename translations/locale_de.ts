32eb245f3191c0ca65733c96b3306b473cf402a2
Snake Amadeus <snake4y5h@gmail.com>
2022-01-04 22:43:34 +0800

    Update Translations
    Updates Translations for Chinese Language.
    Updates internal format blank translations.

--- translations/locale_de.ts
+++ translations/locale_de.ts
@@ -4,84 +4,96 @@
 <context>
     <name>lyricsHelper</name>
     <message>
-        <location filename="../lyricsHelper.qml" line="22"/>
+        <location filename="../lyricsHelper.qml" line="24"/>
         <source>A plugin intends to help input lyrics. It is designed for East Asian languages (monosyllabic languages like Sino-Tibetian languages) but also work for other language texts with workarounds.
 Github Page: https://github.com/SnakeAmadeus/lyricsHelper
 by Snake4y5h</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="54"/>
+        <location filename="../lyricsHelper.qml" line="68"/>
         <source>Current File:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="72"/>
+        <location filename="../lyricsHelper.qml" line="52"/>
         <source>Please choose a .txt or .lrc file</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="883"/>
+        <location filename="../lyricsHelper.qml" line="98"/>
+        <source>Auto Loading Lyrics...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lyricsHelper.qml" line="116"/>
+        <source>Couldn&apos;t find any .txt file contains
+the Current Score&apos;s name in the same path.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lyricsHelper.qml" line="942"/>
+        <location filename="../lyricsHelper.qml" line="957"/>
         <source>Click &quot;...&quot; to open a .txt file→</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="894"/>
+        <location filename="../lyricsHelper.qml" line="970"/>
         <source>Tips：Right Click &quot;...&quot; to open Plugin Settings⚙</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="919"/>
+        <location filename="../lyricsHelper.qml" line="995"/>
         <source>This is not a text for translation but just a notice: you may also need to change settingsPopup.width to adapt to your language&apos;s text space.</source>
         <extracomment>Please check settingsPopup.width at this message&apos;s line in lyricsHelper.qml. You will find a switch() then add your language&apos;s case.</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="947"/>
+        <location filename="../lyricsHelper.qml" line="1023"/>
         <source>&lt;b style=&quot;font-size:8vw&quot;&gt;⚙ Plugin Settings:&lt;/b&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="952"/>
+        <location filename="../lyricsHelper.qml" line="1028"/>
         <source>Replace Mode:
 overwrites existed lyrics</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="956"/>
+        <location filename="../lyricsHelper.qml" line="1032"/>
         <source>🔊Preview Note Sounds</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="960"/>
+        <location filename="../lyricsHelper.qml" line="1036"/>
         <source>Maximum Undo Steps:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="975"/>
+        <location filename="../lyricsHelper.qml" line="1051"/>
         <source>OK</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="1022"/>
+        <location filename="../lyricsHelper.qml" line="1098"/>
         <source>Add
 Syllable</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="1031"/>
+        <location filename="../lyricsHelper.qml" line="1107"/>
         <source>Extend
 Melisma</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="1040"/>
+        <location filename="../lyricsHelper.qml" line="1116"/>
         <source>Concatenate
 Synalepha</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="1068"/>
+        <location filename="../lyricsHelper.qml" line="1144"/>
         <source>Please load a lyrics file first</source>
         <translation type="unfinished"></translation>
     </message>
--- translations/locale_zh.ts
+++ translations/locale_zh.ts
@@ -4,7 +4,7 @@
 <context>
     <name>lyricsHelper</name>
     <message>
-        <location filename="../lyricsHelper.qml" line="23"/>
+        <location filename="../lyricsHelper.qml" line="24"/>
         <source>A plugin intends to help input lyrics. It is designed for East Asian languages (monosyllabic languages like Sino-Tibetian languages) but also work for other language texts with workarounds.
 Github Page: https://github.com/SnakeAmadeus/lyricsHelper
 by Snake4y5h</source>
@@ -12,81 +12,94 @@ by Snake4y5h</source>
 by Snake4y5h</translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="55"/>
+        <location filename="../lyricsHelper.qml" line="68"/>
         <source>Current File:</source>
         <translation>当前歌词：</translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="73"/>
+        <location filename="../lyricsHelper.qml" line="52"/>
         <source>Please choose a .txt or .lrc file</source>
         <translation>请选择一个.txt文件</translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="884"/>
+        <location filename="../lyricsHelper.qml" line="98"/>
+        <source>Auto Loading Lyrics...</source>
+        <translation>自动搜寻歌词文件中...</translation>
+    </message>
+    <message>
+        <location filename="../lyricsHelper.qml" line="116"/>
+        <source>Couldn&apos;t find any .txt file contains
+the Current Score&apos;s name in the same path.</source>
+        <translation>没有在当前乐谱目录下
+找到带有当前乐谱名字的.txt文件.</translation>
+    </message>
+    <message>
+        <location filename="../lyricsHelper.qml" line="942"/>
+        <location filename="../lyricsHelper.qml" line="957"/>
         <source>Click &quot;...&quot; to open a .txt file→</source>
         <translation>先点这儿打开一个歌词文件→</translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="895"/>
+        <location filename="../lyricsHelper.qml" line="970"/>
         <source>Tips：Right Click &quot;...&quot; to open Plugin Settings⚙</source>
         <translation>提示：鼠标右键点击&quot;...&quot;可以打开插件设置⚙</translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="920"/>
+        <location filename="../lyricsHelper.qml" line="995"/>
         <source>This is not a text for translation but just a notice: you may also need to change settingsPopup.width to adapt to your language&apos;s text space.</source>
         <extracomment>Please check settingsPopup.width at this message&apos;s line in lyricsHelper.qml. You will find a switch() then add your language&apos;s case.</extracomment>
         <translation></translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="948"/>
+        <location filename="../lyricsHelper.qml" line="1023"/>
         <source>&lt;b style=&quot;font-size:8vw&quot;&gt;⚙ Plugin Settings:&lt;/b&gt;</source>
         <translation>&lt;b style=&quot;font-size:8vw&quot;&gt;⚙ 插件设置 Settings：&lt;/b&gt;</translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="953"/>
+        <location filename="../lyricsHelper.qml" line="1028"/>
         <source>Replace Mode:
 overwrites existed lyrics</source>
         <translation>替换模式:
 如已有歌词则覆盖</translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="957"/>
+        <location filename="../lyricsHelper.qml" line="1032"/>
         <source>🔊Preview Note Sounds</source>
         <translation>🔊预览音符声音</translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="961"/>
+        <location filename="../lyricsHelper.qml" line="1036"/>
         <source>Maximum Undo Steps:</source>
         <translation>最大撤销步数：</translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="976"/>
+        <location filename="../lyricsHelper.qml" line="1051"/>
         <source>OK</source>
         <translation>OK</translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="1023"/>
+        <location filename="../lyricsHelper.qml" line="1098"/>
         <source>Add
 Syllable</source>
         <translation>单音
 Syllable</translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="1032"/>
+        <location filename="../lyricsHelper.qml" line="1107"/>
         <source>Extend
 Melisma</source>
         <translation>转音
 Melisma</translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="1041"/>
+        <location filename="../lyricsHelper.qml" line="1116"/>
         <source>Concatenate
 Synalepha</source>
         <translation>多音
 Synalepha</translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="1069"/>
+        <location filename="../lyricsHelper.qml" line="1144"/>
         <source>Please load a lyrics file first</source>
         <translation>请先选择一个歌词文件</translation>
     </message>
