fd61e4cbbf82db6fa857159015f6ab6890468133
Snake Amadeus <snake4y5h@gmail.com>
2022-01-13 09:53:10 +0800

    Update Translation
    Update Translation

--- translations/locale_zh.ts
+++ translations/locale_zh.ts
@@ -4,7 +4,7 @@
 <context>
     <name>lyricsHelper</name>
     <message>
-        <location filename="../lyricsHelper.qml" line="24"/>
+        <location filename="../lyricsHelper.qml" line="23"/>
         <source>A plugin intends to help input lyrics. It is designed for East Asian languages (monosyllabic languages like Sino-Tibetian languages) but also work for other language texts with workarounds.
 Github Page: https://github.com/SnakeAmadeus/lyricsHelper
 by Snake4y5h</source>
@@ -16,122 +16,171 @@ by Snake4y5h</translation>
         <translation type="vanished">当前歌词：</translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="57"/>
+        <location filename="../lyricsHelper.qml" line="56"/>
         <source>Please choose a .txt or .lrc file</source>
         <translation>请选择一个.txt文件</translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="73"/>
+        <location filename="../lyricsHelper.qml" line="72"/>
         <source>Current File: </source>
         <translation>当前歌词：</translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="109"/>
+        <location filename="../lyricsHelper.qml" line="115"/>
         <source>Auto Loading Lyrics...</source>
         <translation>自动搜寻歌词文件中...</translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="129"/>
+        <location filename="../lyricsHelper.qml" line="135"/>
         <source>Couldn&apos;t find any .txt file contains
 the Current Score&apos;s name in the same path.</source>
         <translation>没有在当前乐谱目录下
 找到带有当前乐谱名字的.txt文件.</translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="974"/>
-        <location filename="../lyricsHelper.qml" line="992"/>
+        <location filename="../lyricsHelper.qml" line="980"/>
+        <location filename="../lyricsHelper.qml" line="998"/>
         <source>Click &quot;...&quot; to open a .txt file→</source>
         <translation>先点这儿打开一个歌词文件→</translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="1005"/>
+        <location filename="../lyricsHelper.qml" line="1011"/>
         <source>Tips：Right Click &quot;...&quot; to open Plugin Settings⚙</source>
         <translation>提示：鼠标右键点击&quot;...&quot;可以打开插件设置⚙</translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="1050"/>
+        <location filename="../lyricsHelper.qml" line="1056"/>
         <source>&lt;b style=&quot;font-size:8vw&quot;&gt;⚙ Plugin Settings:&lt;/b&gt;</source>
         <translation>&lt;b style=&quot;font-size:8vw&quot;&gt;⚙ 插件设置 Settings：&lt;/b&gt;</translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="1055"/>
+        <location filename="../lyricsHelper.qml" line="1061"/>
         <source>Replace Mode:
 overwrites existed lyrics</source>
         <translation>替换模式:
 如已有歌词则覆盖</translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="1059"/>
+        <location filename="../lyricsHelper.qml" line="1065"/>
         <source>🔊Preview Note Sounds</source>
         <translation>🔊预览音符声音</translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="1063"/>
+        <location filename="../lyricsHelper.qml" line="1069"/>
         <source>Maximum Undo Steps:</source>
         <translation>最大撤销步数：</translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="1078"/>
+        <location filename="../lyricsHelper.qml" line="1084"/>
         <source>OK</source>
         <translation>OK</translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="1124"/>
+        <location filename="../lyricsHelper.qml" line="1130"/>
         <source>Add
 Syllable</source>
         <translation>单音
 Syllable</translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="1133"/>
+        <location filename="../lyricsHelper.qml" line="1139"/>
         <source>Extend
 Melisma</source>
         <translation>转音
 Melisma</translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="1142"/>
+        <location filename="../lyricsHelper.qml" line="1148"/>
         <source>Concatenate
 Synalepha</source>
         <translation>多音
 Synalepha</translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="1172"/>
+        <location filename="../lyricsHelper.qml" line="1178"/>
         <source>Please load a lyrics file first</source>
         <translation>请先选择一个歌词文件</translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="1284"/>
+        <location filename="../lyricsHelper.qml" line="1308"/>
         <source>Hyphenated Mode: </source>
         <translation>英文分词模式：</translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="1291"/>
+        <location filename="../lyricsHelper.qml" line="1315"/>
         <source>English Hyphenation
 (requires Internet connection)</source>
         <translation>英文歌词音节分离
 （需要联网）</translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="1316"/>
+        <location filename="../lyricsHelper.qml" line="1368"/>
+        <source>Japanese Kanji to Kana ▶
+(requires Internet connection)</source>
+        <translation>日语汉字转假名
+（需要联网）</translation>
+    </message>
+    <message>
+        <location filename="../lyricsHelper.qml" line="1369"/>
+        <source>Japanese Kanji to Kana ▶
+(No Japanese Detected)</source>
+        <translation>日语汉字转假名
+（未在歌词里发现日语）</translation>
+    </message>
+    <message>
+        <location filename="../lyricsHelper.qml" line="1382"/>
+        <source>Converting Kanjis to Kana
+Sending Request https://api.kuroshiro.org/convert...</source>
+        <translation>正在进行汉字转假名...
+向https://api.kuroshiro.org/convert服务器发送请求中...</translation>
+    </message>
+    <message>
+        <location filename="../lyricsHelper.qml" line="1401"/>
+        <source>✔ Convertion Completed!</source>
+        <translation>✔ 转换完成！</translation>
+    </message>
+    <message>
+        <location filename="../lyricsHelper.qml" line="1419"/>
+        <source>to Hiragana あ</source>
+        <translation>转成平假名 あ</translation>
+    </message>
+    <message>
+        <location filename="../lyricsHelper.qml" line="1420"/>
+        <source>to Katakana ア</source>
+        <translation>转成片假名 ア</translation>
+    </message>
+    <message>
+        <location filename="../lyricsHelper.qml" line="1422"/>
+        <source>Revert Conversion</source>
+        <translation>还原之前的转换</translation>
+    </message>
+    <message>
+        <location filename="../lyricsHelper.qml" line="1231"/>
         <source>❌ Request timed out</source>
         <translation>❌ 请求超时</translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="1327"/>
+        <location filename="../lyricsHelper.qml" line="1316"/>
+        <source>English Hyphenation
+(No English Detected)</source>
+        <translation>英文歌词音节分离
+（未在歌词里发现英语）</translation>
+    </message>
+    <message>
+        <location filename="../lyricsHelper.qml" line="1338"/>
         <source>Hyphenating Lyrics...
 Sending Request to juiciobrennan.com/syllables/...</source>
         <translation>正在进行单词音节分离...
 向juiciobrennan.com/syllables/服务器发送请求中...</translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="1338"/>
+        <location filename="../lyricsHelper.qml" line="1349"/>
+        <location filename="../lyricsHelper.qml" line="1393"/>
         <source>❌ Connection Failed</source>
         <translation>❌ 联网失败</translation>
     </message>
     <message>
-        <location filename="../lyricsHelper.qml" line="1345"/>
+        <location filename="../lyricsHelper.qml" line="1356"/>
         <source>✔ Hyphenation Completed!</source>
         <translation>✔ 音节分离完成！</translation>
     </message>
